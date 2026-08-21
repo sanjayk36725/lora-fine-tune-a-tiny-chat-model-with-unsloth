5f8675835ecd2ce8900463713748f5c27dcb1361
sanjayk36725 <sanjaykumargovindharaj@gmail.com>
2026-08-21 15:24:26 +0530

    Step 6: attach_lora_adapters

--- model.py
+++ model.py
@@ -44,8 +44,26 @@ def ensure_pad_token(tokenizer):
 def get_lora_target_modules():
     return ["q_proj", "k_proj", "v_proj", "o_proj"]
 
-# Step 6 - attach_lora_adapters (not yet solved)
-# TODO: implement
+# Step 6 - attach_lora_adapters
+def attach_lora_adapters(
+    model,
+    r=8,
+    lora_alpha=16,
+    target_modules=None
+):
+    if target_modules is None:
+        target_modules = get_lora_target_modules()
+
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r=r,
+        target_modules=target_modules,
+        lora_alpha=lora_alpha,
+        lora_dropout=0,
+        bias="none",
+    )
+
+    return model
 
 # Step 7 - count_trainable_parameters (not yet solved)
 # TODO: implement
